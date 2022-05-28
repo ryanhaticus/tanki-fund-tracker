79f52d1934e48d75644629c622824bf244c53fdc
ryanhaticus <ryan@haticus.com>
2022-05-27 22:23:15 -0500

    JSON pisses me off.

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,17 +1,13 @@
-import admin, { ServiceAccount } from 'firebase-admin';
-
-const serviceAccount = `{
-  "projectId": "${process.env.FIREBASE_PROJECT_ID}",
-  "privateKey": "${process.env.FIREBASE_PRIVATE_KEY}",
-  "clientEmail": "${process.env.FIREBASE_CLIENT_EMAIL}"
-}`;
+import admin from 'firebase-admin';
 
 export const useFirebaseOnServer = () => {
   if (admin.apps.length === 0) {
     const app = admin.initializeApp({
-      credential: admin.credential.cert(
-        JSON.parse(serviceAccount) as ServiceAccount,
-      ),
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      }),
       databaseURL: 'https://fund-tracker-68a7b.firebaseio.com',
     });
     return app;
