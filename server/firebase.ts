c3a463a3edc03960b4d6d5011103d483a4b640a9
ryanhaticus <ryan@haticus.com>
2022-05-27 22:03:10 -0500

    Broken in production and now fixed.

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,15 +1,17 @@
 import admin, { ServiceAccount } from 'firebase-admin';
 
-const serviceAccount = {
-  projectId: '${process.env.FIREBASE_PROJECT_ID}',
-  privateKey: '${process.env.FIREBASE_PRIVATE_KEY}',
-  clientEmail: '${process.env.FIREBASE_CLIENT_EMAIL}',
-};
+const serviceAccount = `{
+  "projectId": "${process.env.FIREBASE_PROJECT_ID}",
+  "privateKey": "${process.env.FIREBASE_PRIVATE_KEY}",
+  "clientEmail": "${process.env.FIREBASE_CLIENT_EMAIL}"
+}`;
 
 export const useFirebaseOnServer = () => {
   if (admin.apps.length === 0) {
     const app = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as ServiceAccount),
+      credential: admin.credential.cert(
+        JSON.parse(serviceAccount) as ServiceAccount,
+      ),
       databaseURL: 'https://fund-tracker-68a7b.firebaseio.com',
     });
     return app;
