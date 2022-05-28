189ab3ab6461fff45c4091676c55e9050876555b
ryanhaticus <ryan@haticus.com>
2022-05-27 21:42:47 -0500

    Okay, now it's really fixed!

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,23 +1,15 @@
-import admin from 'firebase-admin';
+import admin, { ServiceAccount } from 'firebase-admin';
 
 const serviceAccount = {
-  type: '${process.env.FIREBASE_ACCOUNT_TYPE}',
-  project_id: '${process.env.FIREBASE_PROJECT_ID}',
-  private_key_id: '${process.env.FIREBASE_PRIVATE_KEY_ID}',
-  private_key: '${process.env.FIREBASE_PRIVATE_KEY}',
-  client_email: '${process.env.FIREBASE_CLIENT_EMAIL}',
-  client_id: '${process.env.FIREBASE_CLIENT_ID}',
-  auth_uri: '${process.env.FIREBASE_AUTH_URI}',
-  token_uri: '${process.env.FIREBASE_TOKEN_URI}',
-  auth_provider_x509_cert_url:
-    '${process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL}',
-  client_x509_cert_url: '${process.env.FIREBASE_CLIENT_X509_CERT_URL}',
+  projectId: '${process.env.FIREBASE_PROJECT_ID}',
+  privateKey: '${process.env.FIREBASE_PRIVATE_KEY}',
+  clientEmail: '${process.env.FIREBASE_CLIENT_EMAIL}',
 };
 
 export const useFirebaseOnServer = () => {
   if (admin.apps.length === 0) {
     const app = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAccount as ServiceAccount),
       databaseURL: 'https://fund-tracker-68a7b.firebaseio.com',
     });
     return app;
