696a55cd929f2ee2e0c3e37e930dc112f2bebab6
ryanhaticus <ryan@haticus.com>
2022-05-27 23:18:44 -0500

    Hid x-axis labels!

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,6 +31,13 @@ const Index = () => {
       <div className='flex gap-x-4 flex-col items-center gap-y-4 lg:flex-row'>
         <div className='flex-grow w-full lg:w-1/2'>
           <Line
+            options={{
+              scales: {
+                x: {
+                  display: false,
+                },
+              },
+            }}
             data={{
               labels: tankoins.map((t) => {
                 const date = new Date(t.date);
@@ -52,6 +59,13 @@ const Index = () => {
         </div>
         <div className='flex-grow w-full lg:w-1/2'>
           <Line
+            options={{
+              scales: {
+                x: {
+                  display: false,
+                },
+              },
+            }}
             data={{
               labels: tankoins.map((t) => {
                 const date = new Date(t.date);
